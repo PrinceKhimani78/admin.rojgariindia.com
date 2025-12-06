6859ab7847475bef9e0cbcc05814aca41fe339a1
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-07 00:54:03 +0530

    aaa

--- mydestiny/controller/application.php
+++ mydestiny/controller/application.php
@@ -1,181 +1,118 @@
+cat > /home/admin.rojgariindia.com/public_html/mydestiny/controller/application.php << 'EOF'
 <?php
-include('../controller/databaseclass.php');
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/rojgar-india/controller/databaseclass.php";
+
 class Application extends Databaseclass
 {
     function __construct()
     {
         parent::__construct();
     }
+
+    function validarecaptcha($response)
+    {
+        if (!empty($response)) {
+            $data = [
+                'secret' => "6Le7gFoUAAAAAJOhjIdEyHaqrli_CTwB7TCHDqRV",
+                'response' => $response
+            ];
+
+            $verify = curl_init();
+            curl_setopt($verify, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+            curl_setopt($verify, CURLOPT_POST, true);
+            curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($verify, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+
+            $response = curl_exec($verify);
+            $responseData = json_decode($response);
+
+            return $responseData->success ?? false;
+        }
+
+        return false;
+    }
+
+    function getpagedetail($pagekey)
+    {
+        if (!empty($pagekey)) {
+            $check = $this->select(['count(id) as page'], 'webpages', ['page_key' => $pagekey], 'first');
+            if ($check['page'] == 0) $pagekey = 'home';
+
+            return $this->select(['all'], 'webpages', ['page_key' => $pagekey], 'first');
+        }
+    }
+
     function checklogin()
     {
-        if(isset($_SESSION['user']) && $_SESSION['user']!='')
-            return true;
-        else
-            return false;
+        return !empty($_SESSION['uid']);
     }
+
     function get_job_industry()
     {
-        return $this->select(array('all'),'job_industry',array('status' => 'Active'));
+        return $this->select(['all'], 'job_industry', ['status' => 'Active']);
     }
+
     function get_job_functions()
     {
-        return $this->select(array('all'),'job_functions',array('status' => 'Active'));
+        return $this->select(['all'], 'job_functions', ['status' => 'Active']);
     }
+
     function get_job_skills()
     {
-        return $this->select(array('all'),'job_skills',array('status' => 'Active'));
+        return $this->select(['all'], 'job_skills', ['status' => 'Active']);
     }
+
     function get_country()
     {
-        return $this->select(array('id','name'),'countries');
+        return $this->select(['id','name'], 'countries');
     }
+
     function get_states($id='')
     {
-        if(isset($id) && is_numeric($id))
-        {
-            return $this->select(array('id','name'),'states',array('country_id' => $id));
-        }
-        else
-        {
-            return $this->select(array('id','name'),'states');
-        }
+        return is_numeric($id)
+            ? $this->select(['id','name'],'states',['country_id'=>$id])
+            : $this->select(['id','name'],'states');
     }
+
     function get_cities($id='')
     {
-        if(isset($id) && is_numeric($id))
-        {
-            return $this->select(array('id','name'),'cities',array('state_id' => $id));
-        }
-        else
-        {
-            return $this->select(array('id','name'),'cities');
-        }
+        return is_numeric($id)
+            ? $this->select(['id','name'],'cities',['state_id'=>$id])
+            : $this->select(['id','name'],'cities');
     }
+
+    function get_city_name($id)
+    {
+        return $this->select(['id','name'],'cities',['id'=>$id],'first');
+    }
+
     function encode_pass($password)
     {
         $algo = $this->GetAlgo();
-        $pass_split = str_split($password);
-        $encoded_pass = '';
-        foreach($pass_split as $key => $passvalue)
-        {
-            $encoded_pass .= $algo[$passvalue] . ":";
-        }
-        return rtrim($encoded_pass, ":");
+        return implode(":", array_map(fn($c) => $algo[$c], str_split($password)));
     }
+
     function decode_pass($hash)
     {
         $return = '';
         $algo = $this->GetAlgo();
-        $unhash = explode(":", $hash);
-        foreach ($unhash as $value)
-        {
+        foreach (explode(":", $hash) as $value) {
             $find = array_search($value, $algo);
-            if ($find)
-                $return .= $find;
+            if ($find) $return .= $find;
         }
         return $return;
     }
+
     function GetAlgo()
     {
         return [
-            "a"=>"H",
-            "b"=>"He",
-            "c"=>"Li",
-            "d"=>"Be",
-            "e"=>"B",
-            "f"=>"C",
-            "g"=>"N",
-            "h"=>"O",
-            "i"=>"F",
-            "j"=>"Ne",
-            "k"=>"Na",
-            "l"=>"Mg",
-            "m"=>"Al",
-            "n"=>"Si",
-            "o"=>"P",
-            "p"=>"S",
-            "q"=>"Cl",
-            "r"=>"Ar",
-            "s"=>"K",
-            "t"=>"Ca",
-            "u"=>"Sc",
-            "v"=>"Ti",
-            "w"=>"V",
-            "x"=>"Cr",
-            "y"=>"Mn",
-            "z"=>"Fe",
-            "A"=>"Co",
-            "B"=>"Ni",
-            "C"=>"Cu",
-            "D"=>"Zn",
-            "E"=>"Ga",
-            "F"=>"Ge",
-            "G"=>"As",
-            "H"=>"Se",
-            "I"=>"Br",
-            "J"=>"Kr",
-            "K"=>"Rb",
-            "L"=>"Sr",
-            "M"=>"Y",
-            "N"=>"Zr",
-            "O"=>"Nb",
-            "P"=>"Mo",
-            "Q"=>"Tc",
-            "R"=>"Ru",
-            "S"=>"Rh",
-            "T"=>"Pd",
-            "U"=>"Ag",
-            "V"=>"Cd",
-            "W"=>"In",
-            "X"=>"Sn",
-            "Y"=>"Sb",
-            "Z"=>"Te",
-            " "=>"I",
-            "1"=>"Xe",
-            "2"=>"Cs",
-            "3"=>"Ba",
-            "4"=>"Hf",
-            "5"=>"Ta",
-            "6"=>"W",
-            "7"=>"Re",
-            "8"=>"Os",
-            "9"=>"Ir",
-            "0"=>"Pt",
-            "!"=>"Au",
-            "£"=>"Hg",
-            "$"=>"Tl",
-            "%"=>"Pb",
-            "^"=>"Bi",
-            "&"=>"Po",
-            "*"=>"At",
-            "("=>"Rn",
-            ")"=>"Fr",
-            "_"=>"Ra",
-            "-"=>"Rf",
-            "="=>"Db",
-            "+"=>"Sg",
-            "`"=>"Bh",
-            "¬"=>"Hs",
-            ""=>"Mt",
-            ","=>"Ds",
-            "<"=>"Rg",
-            "."=>"Cn",
-            ">"=>"Uut",
-            "/"=>"Fl",
-            "?"=>"Uup",
-            ";"=>"Lv",
-            ":"=>"Uus",
-            "'"=>"Uuo",
-            "@"=>"La",
-            "#"=>"Ce",
-            "~"=>"Pr",
-            "["=>"Nd",
-            "]"=>"Pm",
-            "{"=>"Sm",
-            "}"=>"Eu",
-            "."=>"Gd",
-            "\""=>"Jpj",
+            "a"=>"H","b"=>"He","c"=>"Li","d"=>"Be","e"=>"B","f"=>"C","g"=>"N","h"=>"O","i"=>"F","j"=>"Ne",
+            "k"=>"Na","l"=>"Mg","m"=>"Al","n"=>"Si","o"=>"P","p"=>"S","q"=>"Cl","r"=>"Ar","s"=>"K","t"=>"Ca",
+            "u"=>"Sc","v"=>"Ti","w"=>"V","x"=>"Cr","y"=>"Mn","z"=>"Fe","A"=>"Co","B"=>"Ni","C"=>"Cu","D"=>"Zn"
         ];
     }
 }
 ?>
+EOF
